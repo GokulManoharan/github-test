6d3731e7a1297b97e639312d9dd8794840fc95f0
Gokul Manoharan <gokool2u@gmail.com>
2023-09-21 16:32:14 +0530

    Changed text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>This is a new sample PeakZi app</h3>
+        <h3>This is a sample PeakZi app</h3>
       </header>
     </div>
   );
