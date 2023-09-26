a1a9fb066b844e7670d2a3d40e503f08f77f6c03
Gokul Manoharan <gokool2u@gmail.com>
2023-09-26 12:30:34 +0530

    First yaml commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3>This is a sample PeakZi app</h3>
+
+        <h6>v1.0</h6>
       </header>
     </div>
   );
